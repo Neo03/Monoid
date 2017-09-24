3a5932ea16b7828c2d3c73c67863dd6df275445a
Василий Симаков <vasiliy@sim.papa>
2017-09-24 21:29:51 +0300

    Added Trivial Semigrioup instance

--- src/Trivial.hs
+++ src/Trivial.hs
@@ -6,7 +6,12 @@ import Data.Semigroup(Semigroup,(<>))
 data Trivial = Trivial deriving (Eq, Show)
 
 instance Semigroup Trivial where
-  _ <> _ = undefined
+  _ <> _ = Trivial
 
 instance Arbitrary Trivial where
   arbitrary = return  Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,10 @@
 import FirstMappend
 import Test.QuickCheck
+import Trivial
 
 main :: IO ()
 main = do
   quickCheck (monoidAssoc :: FirstMappend)
   quickCheck (monoidLeftIdentity :: First' String -> Bool)
   quickCheck (monoidRightIdentity :: First' String -> Bool)
+  quickCheck (semigroupAssoc :: TrivialAssoc)
